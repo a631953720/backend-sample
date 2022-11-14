2d169d3fe2bc01747d3e5f258c9f0ef4c33e4063
KK <51738970+a631953720@users.noreply.github.com>
2022-11-14 22:42:59 +0800

    update sample

--- C2-2/index.js
+++ C2-2/index.js
@@ -8,16 +8,17 @@ const port = 4567;
 // read update delete...
 app.use(express.json());
 
+// Tips: 可以用下面的方式來做簡單測試
+console.log(getUserPsw('andy')); // 會得到密碼 1234562
+
 app.put('/data', (req, res) => {
   // 真實情況會複雜許多，此範例是為了簡單演示
   const { userName, psw, key, value } = req.body;
-  
-  if (psw === getUserPsw(userName)) {
-    const result = createOrUpdateData(key, value);
-    if (result) return res.status(200).send('ok');
-    else return res.status(500).send('error');
-  }
-  return res.status(401).send('user or psw error');
+
+  console.log({ userName, psw, key, value });
+
+  // 補上登入檢察功能
+  return res.status(200).send('ok');
 });
 
 app.listen(port, () => {
